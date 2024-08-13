97b2b2805a708aaaf4762f248756429c09746826
Sgal Cheung <zhangsigao@live.com>
2024-08-13 19:14:30 +0800

    Upgrade to Astro latest is 4.13.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sanity from 'astro-sanity'
-import image from '@astrojs/image'
 
 export default defineConfig({
   integrations: [
@@ -12,9 +11,6 @@ export default defineConfig({
       dataset: 'production',
       useCdn: false,
       apiVersion: '2021-03-25' // use a UTC date string
-    }),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
     })
   ],
 
