dbf87afccf8492945f73c5f6d37b6fc854bbe7c3
Sgal Cheung <zhangsigao@live.com>
2024-08-17 04:13:10 +0800

    Adjust to server-side rendering type hybrid, and configure adapter to Cloudflare.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  output: 'hybrid',
+  integrations: [tailwind(), icon()],
+  adapter: cloudflare({
+    // [WARN] The currently selected adapter `@astrojs/cloudflare` is not compatible with the image service "Sharp".
+    imageService: 'cloudflare'
+  })
 });
