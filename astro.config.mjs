3b1b461e294ba485ceae4a2edd5bac07d22584dd
Sgal Cheung <zhangsigao@live.com>
2024-08-18 01:28:52 +0800

    Configure astro-icon to only include used icons.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,17 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  integrations: [tailwind(), icon()],
+  output: "hybrid",
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        fluent: ["people-community-16-filled", "timer-16-filled"],
+        ic: ["baseline-plus"],
+        ph: ["cooking-pot"],
+      },
+    }),
+  ],
   adapter: cloudflare({
     // [WARN] The currently selected adapter `@astrojs/cloudflare` is not compatible with the image service "Sharp".
     imageService: 'cloudflare'
