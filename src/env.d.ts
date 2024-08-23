3f09524c35a2eab45acad7cecb0bff6e9f8afa0c
Sgal Cheung <zhangsigao@live.com>
2024-08-23 17:07:55 +0800

    Removed: astro/client-image auto-conversion(https://docs.astro.build/en/guides/upgrade-to/v4/#removed-astroclient-image-auto-conversion).

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,5 @@
 /// <reference types="astro/client" />
 
-// Replace `astro/client` with `@astrojs/image/client`
-/// <reference types="@astrojs/image/client" />
-
 interface ImportMetaEnv {
   readonly SQUIDEX_APP_NAME: string;
   readonly SQUIDEX_ENVIRONMENT: string;
