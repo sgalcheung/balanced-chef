4080a5f4349cb166160df90876c46af28c6186f5
Sgal Cheung <zhangsigao@live.com>
2024-09-04 14:04:39 +0800

    try anagin.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
 import * as process from "node:process";
-import { loadConfig } from "polyfill";
 
 // Dynamically import dotenv functions, only codegen, not Astro development
 // for codegen
@@ -8,7 +7,13 @@ if (process.env.NODE_ENV === "development" && !import.meta.env.DEV) {
   require("dotenv").config();
 }
 
-await loadConfig();
+// Polyfill for Cloudflare
+if (import.meta.env.PROD) {
+  const envClient = await import("astro:env/client");
+  const { SQUIDEX_APP_NAME, SQUIDEX_ENVIRONMENT } = envClient;
+  process.env["SQUIDEX_APP_NAME"] = SQUIDEX_APP_NAME;
+  process.env["SQUIDEX_ENVIRONMENT"] = SQUIDEX_ENVIRONMENT;
+}
 
 interface Config {
   squidexAppName?: string;
